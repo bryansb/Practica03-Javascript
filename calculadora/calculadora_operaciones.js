d993133f768a19b0c631b17661915ef0ac8c4efe
bryansb <51723446+bryansb@users.noreply.github.com>
2020-05-07 19:00:19 -0500

    Correcciones - Calculadora
    Se modifiicó la calculadora; se reemplazó la etiqueta: span, por un input de texto.

--- calculadora/calculadora_operaciones.js
+++ calculadora/calculadora_operaciones.js
@@ -1,26 +1,26 @@
 function init(){
     var answer = document.getElementById('answer');
-    answer.textContent = "0";
+    answer.value = "0";
 }
 
 function writeValue(value){
     var panel = document.getElementById('panel');
-    panel.textContent = panel.textContent + value;
+    panel.value += value;
 }
 
 function clearPanel(){
     var panel = document.getElementById('panel');
     var answer = document.getElementById('answer');
 
-    panel.textContent = " ";
-    answer.textContent = "0";
+    panel.value = " ";
+    answer.value = "0";
 }
 
 function getAnswer(){
     var panel = document.getElementById('panel');
     var answer = document.getElementById('answer');
 
-    var result = eval(panel.textContent);
-    answer.textContent = panel.textContent + "=" + result;
-    panel.textContent = result;
+    var result = eval(panel.value);
+    answer.value = panel.value + "=" + result;
+    panel.value = result;
 }
