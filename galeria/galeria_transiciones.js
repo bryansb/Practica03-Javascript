22df584497edc181f2be581710d4817790b420fe
bryansb <51723446+bryansb@users.noreply.github.com>
2020-05-10 12:35:50 -0500

    Terminado - Galeria
    Pendiende de optimizar y depurar ciertas partes

--- galeria/galeria_transiciones.js
+++ galeria/galeria_transiciones.js
@@ -1,4 +1,5 @@
-var images = ["images/image0.jpg", "images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", "images/image5.jpg", "images/image6.jpg", "images/image7.jpg", "images/image8.jpg", "images/image9.jpg"];
+var images = ["images/image0.jpg", "images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.jpg", 
+                "images/image5.jpg", "images/image6.jpg", "images/image7.jpg", "images/image8.jpg", "images/image9.jpg"];
 var imagesArray = new Array(5);
 var imagesArrayPosition = 0;
 var leftPorcentage = 0;
@@ -6,8 +7,9 @@ var rigthPorcentage = 0;
 
 var control = 0;
 
-var velocidad = 1;
-var velocidad2 = 5;
+var velocidad = 2; // Incremento
+
+var velocidad2 = 1; // Intervalo
 
 function loadImages(){
     imagesArrayPosition = 0;
@@ -32,33 +34,12 @@ function loadImages(){
     for (let index = 0; index < imagesArray.length; index++) {
         imagesArray[index] = images[numbers[index]];
     }
-
-    console.log(imagesArray);
-
+    
     // Se carga la primera imagen y se la visualiza
     document.getElementById("currentImage").src = imagesArray[0];
     document.getElementById("currentImage").style.visibility = 'visible';
     
-
-    // Si se cambia
-    var recorridoDer = document.getElementById("currentImage").style.right.split('%');
-    var recorridoIzq = document.getElementById("currentImage").style.left.split('%');
-
-    console.log('Derecha: '+recorridoDer);
-    console.log('Izquierda: '+recorridoIzq);
-
-    // Resetea los valores de los porcentajes en los parametros left y right
-    /* if ((rigthPorcentage != -25)) {
-        rigthPorcentage = -25;
-        document.getElementById("currentImage").style.right = rigthPorcentage+'%';
-    }
-
-    if (leftPorcentage != 25) {
-        leftPorcentage = 25;
-        document.getElementById("currentImage").style.left = leftPorcentage+'%';
-    } */
     document.getElementById("currentImage").style.right = 0+'%';
-    document.getElementById("currentImage").style.left = 0+'%';
     
     checkArrayPosition();
 }
@@ -67,51 +48,69 @@ var stopRight;
 function previousImage(){
     imagesArrayPosition--;
     checkArrayPosition();
-    document.getElementById("currentImage").style.right = null;
-    stopRight = setInterval(moveRight, velocidad2);
-    //console.log(imagesArrayPosition);
-    //tem();
+    
+    if (!flagG) {
+        stopRight = setInterval(moveRight, 25);
+    }
+    
 }
 
 function moveRight () {
-    /* var recorridoDer = document.getElementById("currentImage").style.right.split('%');
-    var recorridoIzq = document.getElementById("currentImage").style.left.split('%');
-    console.log('Derecha Right: '+recorridoDer);
-    console.log('Izquierda Rigth: '+recorridoIzq); */
-    
-    document.getElementById("currentImage").style.left = incrementLeft(leftPorcentage, velocidad)+'%';
-    if ((leftPorcentage == 50)) {
-        console.log('Igual a 50');
-        //changeImagen();
+    flagG = true;
+    document.getElementById("currentImage").style.right = decrementRigth(rigthPorcentage, velocidad)+'%';
+    if (rigthPorcentage <= -100) {
+        changeImage();
         clearInterval(stopRight);
+        rigthPorcentage = 90;
+        document.getElementById("currentImage").style.right = "90%";
+        stopAux = setInterval(aux2, 25);
+    }
+}
+
+function aux2 () {
+    document.getElementById("currentImage").style.right = decrementRigth(rigthPorcentage, velocidad)+'%';
+    if (rigthPorcentage <= 0) {
+        console.log(rigthPorcentage);
+        flagG = false;
+        clearInterval(stopAux);
+        rigthPorcentage = 0;
+        document.getElementById("currentImage").style.right = "0%";
     }
 }
 
 var stopLeft;
+var flagG = false;
 function nextImage(){
     imagesArrayPosition++;
     checkArrayPosition();
-    document.getElementById("currentImage").style.left = null;
-    stopLeft = setInterval(moveLeft, velocidad2);
-    //stopRight = setInterval(moveRight, velocidad2);
-    //rigthPorcentage = 0;
-    //console.log(imagesArrayPosition);
-    //tem();   
+    
+    if (!flagG){
+        stopLeft = setInterval(moveLeft, 25);
+    }
 }
 
 
-
+var stopAux;
 function moveLeft () {
-    /* var recorridoDer = document.getElementById("currentImage").style.right.split('%');
-    var recorridoIzq = document.getElementById("currentImage").style.left.split('%');
-    console.log('Derecha Left: '+ recorridoDer);
-    console.log('Izquierda Left: '+ recorridoIzq); */
-    
+    flagG = true;
     document.getElementById("currentImage").style.right = incrementRigth(rigthPorcentage, velocidad)+'%';
-    if (rigthPorcentage == 50) {
-        console.log('Igual a 50');
-        //changeImage();
+    if (rigthPorcentage >= 100) {
+        changeImage();
         clearInterval(stopLeft);
+        rigthPorcentage = -90;
+        document.getElementById("currentImage").style.right = "-90%";
+        stopAux = setInterval(aux, 25);
+    }
+}
+
+function aux(){
+    document.getElementById("currentImage").style.right = incrementRigth(rigthPorcentage, velocidad)+'%';
+    if (rigthPorcentage >= 0) {
+        console.log(rigthPorcentage);
+        flagG = false;
+        clearInterval(stopAux);
+        rigthPorcentage = 0;
+        document.getElementById("currentImage").style.right = "0%";
     }
 }
 
@@ -139,21 +138,26 @@ function setTimeZero () {
 }
 
 function setTime () {
-    document.getElementById("currentImage").style.transition = `${4}s`;
+    document.getElementById("currentImage").style.transition = `${2}s`;
 }
-
 
 function incrementRigth (value, step) {
     value += step;
     rigthPorcentage = value;
-    leftPorcentage = (value*-1);
+    return value;
+}
+
+function decrementRigth (value, step) {
+    value -= step;
+    rigthPorcentage = value;
     return value;
 }
 
 function incrementLeft (value, step) {
     value += step;
-    leftPorcentage = value;
-    rigthPorcentage = (value*-1);
+    rigthPorcentage
+    //leftPorcentage = value;
+    //rigthPorcentage = (value*-1);
     return value;
 }
 
@@ -161,14 +165,8 @@ function changeImage(){
     document.getElementById("currentImage").src = imagesArray[imagesArrayPosition];
 }
 
-function tem(){
-    if ((rigthPorcentage != -25)) {
-        rigthPorcentage = -25;
-        document.getElementById("currentImage").style.right = rigthPorcentage+'%';
-    }
-
-    if (leftPorcentage != 25) {
-        leftPorcentage = 25;
-        document.getElementById("currentImage").style.left = leftPorcentage+'%';
+function load(){
+    if (!flagG){
+        loadImages();
     }
 }
